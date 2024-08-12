11230ce8e1af063b440b2c643a35d38ae1dba400
Seokki-Kwon <seokki0706@gmail.com>
2024-08-12 13:05:55 +0900

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by 권석기 on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
